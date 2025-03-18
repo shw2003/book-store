6dd120da65a876b50c434f3bf1e1d65a42e6f1b9
Shweta Kamal <shwetakamal4714@gmail.com>
2025-03-19 01:39:38 +0530

    Update conn.js

--- backend/connection/conn.js
+++ backend/connection/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose
   .connect(
-    "mongodb+srv://kamalshweta9:&Hwetakamal1997@cluster0.xrtd4.mongodb.net/crud?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://<userid>:<password>@cluster0.xrtd4.mongodb.net/crud?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => console.log("mongo DB Connected"));
