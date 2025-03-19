d3579fa338289e6c9b908cf00006cd07d45c3873
shweta2003 <shwetakamal4714@gmail.com>
2025-03-20 02:23:59 +0530

    resolve

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -6,7 +6,7 @@ router.post("/add", async (req, res) => {
   try {
     const data = req.body;
     const newBook = new bookModel(data);
-    
+
     await newBook.save().then(() => {
       res.status(200).json({ message: "BOOK ADDED SUCCESSFULLY" });
     });
